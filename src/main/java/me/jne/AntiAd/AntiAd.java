4af3ef90d4b1307ff9e75a307a4549cce2f990d1
kasper <kasper@franzmedia.dk>
2013-01-19 22:38:32 +0100

    Fixes for the config part, should be better to check if it dosn't exists and not override anything!

--- src/main/java/me/jne/AntiAd/AntiAd.java
+++ src/main/java/me/jne/AntiAd/AntiAd.java
@@ -1,11 +1,11 @@
 package me.jne.AntiAd;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Arrays;
 import java.util.logging.Level;
-import me.jne.AntiAd.ADCommand;
-import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class AntiAd extends JavaPlugin {
@@ -19,6 +19,7 @@ public class AntiAd extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        doConfigThings();
         adfinder = new Adfinder(this);
 
 
@@ -29,11 +30,9 @@ public class AntiAd extends JavaPlugin {
         getCommand("antiad").setExecutor(new ADCommand(this));
 
 
-        final FileConfiguration config = getConfig();
-        config.addDefault("Detected-Commands", Arrays.asList("/msg", "/message", "/tell"));
-        config.addDefault("Stealth-Mode", "true");
-        config.options().copyDefaults(true);
-        saveConfig();
+
+
+
         File whitelistFile = new File(getDataFolder(), "Whitelist.txt");
         if (!whitelistFile.exists()) {
             try {
@@ -65,8 +64,63 @@ public class AntiAd extends JavaPlugin {
         getLogger().info(getDescription().getName() + " " + "Version" + " " + getDescription().getVersion() + " is now Enabled");
 
     }
-
+/**
+ * Get the adfinder :)
+ * @return  The adfiner in this plugin!
+ */
     public Adfinder getAdfinder() {
         return adfinder;
+    }
+/**
+ * This loads the config file and see if it misses any commands if it does it add them (also here we add new ones)!
+ */
+    private void doConfigThings() {
+        //We checks if the config exsists
+        File configFile;
+        configFile = new File(getDataFolder(), "config.yml");
+
+        if (!configFile.exists()) {
+            configFile.getParentFile().mkdirs();
+            copy(getResource("config.yml"), configFile);
+        }
+
+
+        // We check if the config contains this new ones, if it dosn't we add them.
+        if (!getConfig().contains("Detected-Commands")) {
+            getConfig().set("Detected-Commands", Arrays.asList("/msg", "/message", "/tell"));
+            System.out.println("we are saving!");
+            saveConfig();
+        }else{
+            System.out.println("contained dected");
+        }
+        if (!getConfig().contains("Stealth-Mode")) {
+            getConfig().set("Stealth-Mode", "true");
+            System.out.println("we are saving!");
+            saveConfig();
+        }else{
+            System.out.println("contained Stelath");
+        }
+    }
+/**
+ * copying one file to another!
+ * @param resource the file you want to copy from
+ * @param configFile the file you want to copy to.
+ */
+    private void copy(InputStream resource, File configFile) {
+           if (resource != null) {
+                FileOutputStream output = null;
+
+                try {
+                    output = new FileOutputStream(configFile);
+                    byte[] buf = new byte[4096];
+                    int length;
+                    while ((length = resource.read(buf)) > 0) {
+                        output.write(buf, 0, length);
+                    }
+                    getLogger().info("[AntiAD] Loading the Default config: config.yml");
+                }catch (IOException e) {
+                   getLogger().warning("Error while loading the file!!!!!!"+e.getMessage());
+                } 
+           }
     }
 }
