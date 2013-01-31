969d9dae4b419441625da8dffef5c1bf5be5175c
kasper Franz <kasper@franzmedia.dk>
2013-01-31 07:27:31 +0100

    Made some changed to the config
    begining to use checkCOnfig() to see if the config excists and if it
    doesn't we make it with the default

--- src/main/java/me/jne/AntiAd/AntiAd.java
+++ src/main/java/me/jne/AntiAd/AntiAd.java
@@ -1,10 +1,12 @@
 package me.jne.AntiAd;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Arrays;
+import java.util.List;
 import java.util.logging.Level;
-import me.jne.AntiAd.ADCommand;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -28,11 +30,14 @@ public class AntiAd extends JavaPlugin {
         //Setting up the command Executer (antiAD)
         getCommand("antiad").setExecutor(new ADCommand(this));
 
-
+        checkConfig();
         final FileConfiguration config = getConfig();
+        if(!config.contains("Detected-Commands")){
         config.addDefault("Detected-Commands", Arrays.asList("/msg", "/message", "/tell"));
-        config.options().copyDefaults(true);
+        
         saveConfig();
+        }
+        
         File whitelistFile = new File(getDataFolder(), "Whitelist.txt");
         if (!whitelistFile.exists()) {
             try {
@@ -67,5 +72,44 @@ public class AntiAd extends JavaPlugin {
 
     public Adfinder getAdfinder() {
         return adfinder;
+    }
+    
+    private void checkConfig() {
+        String name = "config.yml";
+        File actual = new File(getDataFolder(), name);
+        if (!actual.exists()) {
+            getDataFolder().mkdir();
+            InputStream input = this.getClass().getResourceAsStream(
+                    "/config.yml");
+            if (input != null) {
+                FileOutputStream output = null;
+
+                try {
+                    output = new FileOutputStream(actual);
+                    byte[] buf = new byte[4096]; // [8192]?
+                    int length;
+                    while ((length = input.read(buf)) > 0) {
+                        output.write(buf, 0, length);
+                    }
+                    getLogger().info(" Loading the Default config: " + name);
+                } catch (IOException e) {
+                    getLogger().warning("Error while loading the file!!!!!!"+e.getMessage());
+                } finally {
+                    try {
+                        if (input != null) {
+                            input.close();
+                        }
+                    } catch (IOException e) {
+                    }
+
+                    try {
+                        if (output != null) {
+                            output.close();
+                        }
+                    } catch (IOException e) {
+                    }
+                }
+            }
+        }
     }
 }
