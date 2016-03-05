c100514da2cd6d019e807c1a59b02b07e73efe06
XxCoolGamesxX <follet2004@gmail.com>
2016-03-05 17:21:06 +0100

    Update the pom.xml and Update the Metrics.

--- src/main/java/org/mcstats/MetricsLite.java
+++ src/main/java/org/mcstats/MetricsLite.java
@@ -1,36 +1,38 @@
 /*
-* Copyright 2011-2013 Tyler Blair. All rights reserved.
-*
-* Redistribution and use in source and binary forms, with or without modification, are
-* permitted provided that the following conditions are met:
-*
-* 1. Redistributions of source code must retain the above copyright notice, this list of
-* conditions and the following disclaimer.
-*
-* 2. Redistributions in binary form must reproduce the above copyright notice, this list
-* of conditions and the following disclaimer in the documentation and/or other materials
-* provided with the distribution.
-*
-* THIS SOFTWARE IS PROVIDED BY THE AUTHOR ''AS IS'' AND ANY EXPRESS OR IMPLIED
-* WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
-* FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR
-* CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-* CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-* SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
-* ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-* NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
-* ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*
-* The views and conclusions contained in the software and documentation are those of the
-* authors and contributors and should not be interpreted as representing official policies,
-* either expressed or implied, of anybody else.
-*/
+ * Copyright 2011-2013 Tyler Blair. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are
+ * permitted provided that the following conditions are met:
+ *
+ *    1. Redistributions of source code must retain the above copyright notice, this list of
+ *       conditions and the following disclaimer.
+ *
+ *    2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *       of conditions and the following disclaimer in the documentation and/or other materials
+ *       provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ''AS IS'' AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * The views and conclusions contained in the software and documentation are those of the
+ * authors and contributors and should not be interpreted as representing official policies,
+ * either expressed or implied, of anybody else.
+ */
 
 package org.mcstats;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Server;
 import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.scheduler.BukkitTask;
@@ -42,10 +44,12 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.UnsupportedEncodingException;
+import java.lang.reflect.Method;
 import java.net.Proxy;
 import java.net.URL;
 import java.net.URLConnection;
 import java.net.URLEncoder;
+import java.util.Collection;
 import java.util.UUID;
 import java.util.logging.Level;
 import java.util.zip.GZIPOutputStream;
@@ -53,58 +57,58 @@ import java.util.zip.GZIPOutputStream;
 public class MetricsLite {
 
     /**
-* The current revision number
-*/
+     * The current revision number
+     */
     private final static int REVISION = 7;
 
     /**
-* The base url of the metrics domain
-*/
+     * The base url of the metrics domain
+     */
     private static final String BASE_URL = "http://report.mcstats.org";
 
     /**
-* The url used to report a server's status
-*/
+     * The url used to report a server's status
+     */
     private static final String REPORT_URL = "/plugin/%s";
 
     /**
-* Interval of time to ping (in minutes)
-*/
+     * Interval of time to ping (in minutes)
+     */
     private final static int PING_INTERVAL = 15;
 
     /**
-* The plugin this metrics submits for
-*/
+     * The plugin this metrics submits for
+     */
     private final Plugin plugin;
 
     /**
-* The plugin configuration file
-*/
+     * The plugin configuration file
+     */
     private final YamlConfiguration configuration;
 
     /**
-* The plugin configuration file
-*/
+     * The plugin configuration file
+     */
     private final File configurationFile;
 
     /**
-* Unique server id
-*/
+     * Unique server id
+     */
     private final String guid;
 
     /**
-* Debug mode
-*/
+     * Debug mode
+     */
     private final boolean debug;
 
     /**
-* Lock for synchronization
-*/
+     * Lock for synchronization
+     */
     private final Object optOutLock = new Object();
 
     /**
-* Id of the scheduled task
-*/
+     * Id of the scheduled task
+     */
     private volatile BukkitTask task = null;
 
     public MetricsLite(Plugin plugin) throws IOException {
@@ -135,12 +139,12 @@ public class MetricsLite {
     }
 
     /**
-* Start measuring statistics. This will immediately create an async repeating task as the plugin and send
-* the initial data to the metrics backend, and then after that it will post in increments of
-* PING_INTERVAL * 1200 ticks.
-*
-* @return True if statistics measuring is running, otherwise false.
-*/
+     * Start measuring statistics. This will immediately create an async repeating task as the plugin and send
+     * the initial data to the metrics backend, and then after that it will post in increments of
+     * PING_INTERVAL * 1200 ticks.
+     *
+     * @return True if statistics measuring is running, otherwise false.
+     */
     public boolean start() {
         synchronized (optOutLock) {
             // Did we opt out?
@@ -190,10 +194,10 @@ public class MetricsLite {
     }
 
     /**
-* Has the server owner denied plugin metrics?
-*
-* @return true if metrics should be opted out of it
-*/
+     * Has the server owner denied plugin metrics?
+     *
+     * @return true if metrics should be opted out of it
+     */
     public boolean isOptOut() {
         synchronized (optOutLock) {
             try {
@@ -215,10 +219,10 @@ public class MetricsLite {
     }
 
     /**
-* Enables metrics for the server by setting "opt-out" to false in the config file and starting the metrics task.
-*
-* @throws java.io.IOException
-*/
+     * Enables metrics for the server by setting "opt-out" to false in the config file and starting the metrics task.
+     *
+     * @throws java.io.IOException
+     */
     public void enable() throws IOException {
         // This has to be synchronized or it can collide with the check in the task.
         synchronized (optOutLock) {
@@ -236,10 +240,10 @@ public class MetricsLite {
     }
 
     /**
-* Disables metrics for the server by setting "opt-out" to true in the config file and canceling the metrics task.
-*
-* @throws java.io.IOException
-*/
+     * Disables metrics for the server by setting "opt-out" to true in the config file and canceling the metrics task.
+     *
+     * @throws java.io.IOException
+     */
     public void disable() throws IOException {
         // This has to be synchronized or it can collide with the check in the task.
         synchronized (optOutLock) {
@@ -258,10 +262,10 @@ public class MetricsLite {
     }
 
     /**
-* Gets the File object of the config file that should be used to store data such as the GUID and opt-out status
-*
-* @return the File object for the config file
-*/
+     * Gets the File object of the config file that should be used to store data such as the GUID and opt-out status
+     *
+     * @return the File object for the config file
+     */
     public File getConfigFile() {
         // I believe the easiest way to get the base folder (e.g craftbukkit set via -P) for plugins to use
         // is to abuse the plugin object we already have
@@ -273,10 +277,32 @@ public class MetricsLite {
         // return => base/plugins/PluginMetrics/config.yml
         return new File(new File(pluginsFolder, "PluginMetrics"), "config.yml");
     }
+    
+    /**
+     * Gets the online player (backwards compatibility)
+     * 
+     * @return online player amount
+     */
+    private int getOnlinePlayers() {
+        try {
+            Method onlinePlayerMethod = Server.class.getMethod("getOnlinePlayers");
+            if(onlinePlayerMethod.getReturnType().equals(Collection.class)) {
+                return ((Collection<?>)onlinePlayerMethod.invoke(Bukkit.getServer())).size();
+            } else {
+                return ((Player[])onlinePlayerMethod.invoke(Bukkit.getServer())).length;
+            }
+        } catch (Exception ex) {
+            if (debug) {
+                Bukkit.getLogger().log(Level.INFO, "[Metrics] " + ex.getMessage());
+            }
+        }
+        
+        return 0;
+    }
 
     /**
-* Generic method that posts a plugin to the metrics website
-*/
+     * Generic method that posts a plugin to the metrics website
+     */
     private void postPlugin(boolean isPing) throws IOException {
         // Server software specific section
         PluginDescriptionFile description = plugin.getDescription();
@@ -284,7 +310,7 @@ public class MetricsLite {
         boolean onlineMode = Bukkit.getServer().getOnlineMode(); // TRUE if online mode is enabled
         String pluginVersion = description.getVersion();
         String serverVersion = Bukkit.getVersion();
-        int playersOnline = Bukkit.getServer().getOnlinePlayers().length;
+        int playersOnline = this.getOnlinePlayers();
 
         // END server software specific section -- all code below does not use any code outside of this class / Java
 
@@ -382,11 +408,11 @@ public class MetricsLite {
     }
 
     /**
-* GZip compress a string of bytes
-*
-* @param input
-* @return
-*/
+     * GZip compress a string of bytes
+     *
+     * @param input
+     * @return
+     */
     public static byte[] gzip(String input) {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         GZIPOutputStream gzos = null;
@@ -407,10 +433,10 @@ public class MetricsLite {
     }
 
     /**
-* Check if mineshafter is present. If it is, we need to bypass it to send POST requests
-*
-* @return true if mineshafter is installed on the server
-*/
+     * Check if mineshafter is present. If it is, we need to bypass it to send POST requests
+     *
+     * @return true if mineshafter is installed on the server
+     */
     private boolean isMineshafterPresent() {
         try {
             Class.forName("mineshafter.MineServer");
@@ -421,13 +447,13 @@ public class MetricsLite {
     }
 
     /**
-* Appends a json encoded key/value pair to the given string builder.
-*
-* @param json
-* @param key
-* @param value
-* @throws UnsupportedEncodingException
-*/
+     * Appends a json encoded key/value pair to the given string builder.
+     *
+     * @param json
+     * @param key
+     * @param value
+     * @throws UnsupportedEncodingException
+     */
     private static void appendJSONPair(StringBuilder json, String key, String value) throws UnsupportedEncodingException {
         boolean isValueNumeric = false;
 
@@ -455,11 +481,11 @@ public class MetricsLite {
     }
 
     /**
-* Escape a string to create a valid JSON string
-*
-* @param text
-* @return
-*/
+     * Escape a string to create a valid JSON string
+     *
+     * @param text
+     * @return
+     */
     private static String escapeJSON(String text) {
         StringBuilder builder = new StringBuilder();
 
@@ -501,11 +527,11 @@ public class MetricsLite {
     }
 
     /**
-* Encode text as UTF-8
-*
-* @param text the text to encode
-* @return the encoded text, as UTF-8
-*/
+     * Encode text as UTF-8
+     *
+     * @param text the text to encode
+     * @return the encoded text, as UTF-8
+     */
     private static String urlEncode(final String text) throws UnsupportedEncodingException {
         return URLEncoder.encode(text, "UTF-8");
     }
